f924fd70d00e9fcbc2cb11c5c6b92516927f8dc2
meekamoodz4-hub <meekamoodz4@gmail.com>
2026-09-20 14:11:57 +0200

    Update MainActivity.kt

--- app/src/main/java/com/meekamoodz/spacexzoom/MainActivity.kt
+++ app/src/main/java/com/meekamoodz/spacexzoom/MainActivity.kt
@@ -56,11 +56,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.alpha
-import androidx.compose.ui.clip
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.LayoutDirection
-import androidx.compose.ui.unit.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
